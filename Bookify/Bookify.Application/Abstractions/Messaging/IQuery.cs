93155e504404eb2c19476a92372d2b17a9b62cf8
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2025-07-04 11:16:54 +0000

    feature: set up CQRS

---
+++ Bookify/Bookify.Application/Abstractions/Messaging/IQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bookify.Domain.Entities.Abstractions;
+using MediatR;
+
+namespace Bookify.Application.Abstractions.Messaging;
+
+public interface IQuery<TResponse> : IRequest<Result<TResponse>>
+{
+}
