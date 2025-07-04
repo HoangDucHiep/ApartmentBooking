44e6cf679211b9b9c30b1ca3f067647aabbf890c
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2025-07-04 11:10:14 +0000

    feature: add application layer, DependencyInjection setup

--- Bookify/Bookify.Domain/obj/Debug/net9.0/Bookify.Domain.AssemblyInfo.cs
+++ Bookify/Bookify.Domain/obj/Debug/net9.0/Bookify.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookify.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bd01ccb28c9ae0a01933d1b369ac5758de5655d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f356d4168acbe694ee1eaeebdae306c010b7b124")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookify.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookify.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
