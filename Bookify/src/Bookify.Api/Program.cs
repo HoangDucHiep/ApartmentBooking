d2de6ca0b39cfcce9a35c5e7d3e06802ad2ec726
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2025-07-07 11:31:50 +0000

    feature: add user register and keycloak provider

---
+++ Bookify/src/Bookify.Api/Program.cs
@@ -0,0 +1,34 @@
+using Bookify.Api.Extensions;
+using Bookify.Application;
+using Bookify.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.ApplyMigrations();
+
+    // REMARK: Uncomment if you want to seed initial data.
+    app.SeedData();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCustomExceptionHandler();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Bookify/src/Bookify.Application/Users/RegisterUser/RegisterUserCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace Bookify.Application.Users.RegisterUser;
+
+public class RegisterUserCommandValidator : AbstractValidator<RegisterUserCommand>
+{
+
+    public RegisterUserCommandValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+        RuleFor(x => x.FirstName)
+            .NotEmpty()
+            .MaximumLength(50);
+        RuleFor(x => x.LastName)
+            .NotEmpty()
+            .MaximumLength(50);
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MinimumLength(6)
+            .MaximumLength(100);
+    }
+
+}
